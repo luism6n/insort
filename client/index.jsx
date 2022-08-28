412feb3cf2db87c11341800d76f918f03ec88e45
Luís Möllmann <luism6n@gmail.com>
2022-08-28 00:02:27 -0300

    add rooms

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,56 @@
 import ReactDOM from "react-dom/client";
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+import { nanoid } from "nanoid";
+
+function Home() {
+  return (
+    <p>
+      {/* This has to be and anchor tag, not a Link,
+          so that we actually hit the server */}
+      Get a <a href={`/r/${nanoid()}`}>room</a>
+    </p>
+  );
+}
+
+function Room() {
+  let { roomId } = useParams();
+  const [numPlayers, setNumPlayers] = useState(0);
+  let socket = io();
+
+  useEffect(() => {
+    socket.emit("join", { roomId });
+
+    socket.on("gameState", (data) => {
+      setNumPlayers(data.numPlayers);
+    });
+  }, []);
+
+  return (
+    <p>
+      You're in room {`${roomId}`}{" "}
+      {numPlayers > 1 ? "with " + numPlayers + " others" : "alone"}
+    </p>
+  );
+}
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/r/:roomId" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("App"));
--- server/main.js
+++ server/main.js
@@ -3,12 +3,57 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const rooms = new Map();
+const socketToRoom = new Map();
+
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
 
 let app = express();
+
+app.get("/r/:roomId", (req, res) => {
+  res.sendFile(path.join(publicPath, "/index.html"));
+});
+
 let server = http.createServer(app);
 let io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("join", (data) => {
+    socketToRoom.set(socket.id, data.roomId);
+
+    let currentState = rooms.get(data.roomId);
+    if (!currentState) {
+      currentState = { numPlayers: 1 };
+    } else {
+      currentState.numPlayers++;
+    }
+
+    rooms.set(data.roomId, currentState);
+
+    console.log(`user joined, socketId=${socket.id}, roomId=${data.roomId}`);
+    io.emit("gameState", currentState);
+  });
+
+  socket.on("disconnect", () => {
+    let roomId = socketToRoom.get(socket.id);
+
+    if (!roomId) {
+      console.warn(`disconnect with no room, socketId=${socket.id}`);
+      return;
+    }
+
+    let state = rooms.get(roomId);
+    state.numPlayers--;
+
+    if (state.numPlayers === 0) {
+      rooms.delete(roomId);
+    }
+
+    rooms.set(roomId, state);
+    console.log(`user left, socketId=${socket.id}, roomId=${roomId}`);
+  });
+});
 
 app.use(express.static(publicPath));
 
