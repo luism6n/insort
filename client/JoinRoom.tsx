db62f5656c88c90364dc483c7098319d1e1c5492
Luís Möllmann <luism6n@gmail.com>
2022-09-08 20:25:17 -0300

    submit "join room" form on enter

--- client/JoinRoom.tsx
+++ client/JoinRoom.tsx
@@ -4,13 +4,18 @@ import { Button, TextInput, Title } from "./designSystem";
 export function JoinRoom(props: { join: (playerName: string) => void }) {
   const [nameInput, setNameInput] = useState("");
 
+  function handleJoin(e: React.FormEvent) {
+    e.preventDefault();
+    props.join(nameInput);
+  }
+
   return (
     <div className="flex flex-col justify-center w-1/2">
       <Title>Join Room</Title>
-      <TextInput input={nameInput} setInput={setNameInput} />
-      <div className="flex justify-center">
-        <Button onClick={() => props.join(nameInput)}>Join</Button>
-      </div>
+      <form onSubmit={handleJoin} className="flex justify-center">
+        <TextInput input={nameInput} setInput={setNameInput} />
+        <Button type="submit">Join</Button>
+      </form>
     </div>
   );
 }
