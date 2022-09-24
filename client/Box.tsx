6413269385d8742e216e0a2980ecc9cea0b941c1
Luís Möllmann <luism6n@gmail.com>
2022-09-23 21:20:04 -0300

    make text boxes a little slimmer (looks better on big screens)

--- client/Box.tsx
+++ client/Box.tsx
@@ -9,7 +9,7 @@ export default function Box(props: {
   return (
     <div
       onClick={props.onClick}
-      className="max-w-xl w-full flex flex-col items-center p-2 m-2 border-4 bg-white"
+      className="max-w-md w-full flex flex-col items-center p-2 m-2 border-4 bg-white"
       style={{ borderColor: colors.purple, backgroundColor: props.bgColor }}
     >
       {props.children}
